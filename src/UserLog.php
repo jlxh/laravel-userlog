c35d02677b8477bf27768c4615250d992fe9610b
yaoshanliang <1329517386@qq.com>
2016-08-01 17:12:53 +0800

    add draft

---
+++ src/UserLog.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jlxh\LaravelUserLog;
+
+class UserLog
+{
+    public function __construct()
+    {
+    }
+
+    public static function create()
+    {
+        dd('create');
+    }
+
+    public function run()
+    {
+    }
+
+    public function beforeRun()
+    {
+    }
+
+    public function afterRun()
+    {
+    }
+
+}
