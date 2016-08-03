41b281f1781fd4ee121836cb81477e4e57a8944d
yaoshanliang <1329517386@qq.com>
2016-08-03 17:32:05 +0800

    add queue to log

--- src/UserLog/UserLog.php
+++ src/UserLog/UserLog.php
@@ -5,6 +5,7 @@ namespace Jlxh\UserLog;
 use DB;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Queue;
 
 class UserLog
 {
@@ -14,14 +15,24 @@ class UserLog
 
     public static function create($userId = 0, $title = '', $type = 'S', $data = '', $sql = '')
     {
-        return DB::table(Config::get('userlog.table_name'))->insertGetId([
-            'user_id' => $userId,
-            'title' => $title,
-            'type' => $type,
-            'data' => $data,
-            'sql' => $sql,
-            'ip' => Request::ip()
-        ]);
+        Queue::push(function($job) use ($userId, $title, $type, $data, $sql) {
+            $pushed_at = date('Y-m-d H:i:s');
+            $job->delete();
+            $poped_at = date('Y-m-d H:i:s');
+
+            DB::table(Config::get('userlog.table_name'))->insertGetId([
+                'user_id' => $userId,
+                'title' => $title,
+                'type' => $type,
+                'data' => $data,
+                'sql' => $sql,
+                'ip' => Request::ip(),
+                'pushed_at' => $pushed_at,
+                'poped_at' => $poped_at,
+                'created_at' => date('Y-m-d H:i:s')
+            ]);
+
+        });
     }
 
     public function run()
